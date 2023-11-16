1e8d62dea008bb6d457805f3bab1dc3f2598f3c2
abhits <abhiramits004@gmail.com>
2023-11-16 23:22:39 +0530

    Add Food class with random position generation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
 from turtle import Turtle, Screen
-from snake import Snake  # Assuming you have a Snake class defined in 'snake.py'
+from snake import Snake
+from food import Food
 import time
 
 # Set up the screen
@@ -13,6 +14,9 @@ screen.tracer(0)  # Turns off automatic screen updates
 # Create a snake object
 snake = Snake()
 
+# Create a food object
+food = Food()
+
 # Set up key bindings for snake movement
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
@@ -24,8 +28,11 @@ game_on = True  # Flag to control the game loop
 
 while game_on:
     screen.update()  # Manually update the screen
-    time.sleep(0.5)  # Introduce a delay to control the speed of the game
+    time.sleep(0.1)  # Introduce a delay to control the speed of the game
     snake.move()  # Move the snake
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 # Close the window when clicked
 screen.exitonclick()
