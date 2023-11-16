b900f58a4dd7b1320ff3c332faf34674e33b86fa
abhits <abhiramits004@gmail.com>
2023-11-17 00:11:41 +0530

    Implement add_segments method in Snake class

--- snake.py
+++ snake.py
@@ -1,11 +1,15 @@
 from turtle import Turtle
 
+# Initial positions for the snake segments
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+# Distance to move the snake in each step
 MOVE_DISTANCE = 20
+# Turtle screen directions
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
 
 class Snake:
 
@@ -13,24 +17,33 @@ class Snake:
         # Initialize the snake with starting positions
         self.snake_segments = []
         self.create_snake()
+        # Set the snake head as the first segment
         self.head = self.snake_segments[0]
 
     def create_snake(self):
         # Create the initial snake with three segments
         for position in STARTING_POSITIONS:
-            snake = Turtle()
-            snake.shape("square")
-            snake.color("white")
-            snake.penup()
-            snake.goto(position)
-            self.snake_segments.append(snake)
+            self.add_segments(position)
+
+    def add_segments(self, position):
+        snake = Turtle()
+        snake.shape("square")
+        snake.color("white")
+        snake.penup()
+        snake.goto(position)
+        # Add the new snake segment to the list
+        self.snake_segments.append(snake)
+
+    def extend_segment(self):
+        self.add_segments(self.snake_segments[-1].position())
 
     def move(self):
-        # Move the snake forward
+        # Move the snake forward by updating the positions of each segment
         for seg_num in range(len(self.snake_segments) - 1, 0, -1):
             x_index = self.snake_segments[seg_num - 1].xcor()
             y_index = self.snake_segments[seg_num - 1].ycor()
             self.snake_segments[seg_num].goto(x_index, y_index)
+        # Move the head of the snake forward
         self.head.forward(MOVE_DISTANCE)
 
     def left(self):
