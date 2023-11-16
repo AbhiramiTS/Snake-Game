03c687d90994e452f5de2c2a874b2c4b5f3d0656
abhits <abhiramits004@gmail.com>
2023-11-17 00:09:20 +0530

    Add functionality to display game over message on the scoreboard and exit on click

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import turtle
 from turtle import Turtle, Screen
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 import time
 
 # Set up the screen
@@ -17,6 +18,9 @@ snake = Snake()
 # Create a food object
 food = Food()
 
+# Create a ScoreBoard object to keep track of the score
+score_board = ScoreBoard()
+
 # Set up key bindings for snake movement
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
@@ -31,8 +35,28 @@ while game_on:
     time.sleep(0.1)  # Introduce a delay to control the speed of the game
     snake.move()  # Move the snake
 
+    # Check if the snake has eaten the food
     if snake.head.distance(food) < 15:
-        food.refresh()
+        score_board.increment_score()  # Increase the score
+        food.refresh()  # Move the food to a new random location
+        snake.extend_segment()  # Extend the snake by adding a new segment
+
+    # Detect collision with the wall
+    if (
+            snake.head.xcor() > 280
+            or snake.head.xcor() < -280
+            or snake.head.ycor() > 280
+            or snake.head.ycor() < -280
+    ):
+        game_on = False  # End the game if the snake hits the wall
+
+    # Detect collision with the snake's tail
+    for segment in snake.snake_segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False  # End the game if the snake collides with its tail
+
+# Display the game over message on the scoreboard
+score_board.game_over()
 
 # Close the window when clicked
 screen.exitonclick()
